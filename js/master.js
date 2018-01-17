b230a8ce7f35efb94ee0cfb54315797fd6b0717d
Gianmarco <GSCordova7@gmail.com>
2018-01-17 23:00:40 +0100

    Añadido funcion al boton de header

--- js/master.js
+++ js/master.js
@@ -3,3 +3,11 @@
 function myFunction(){
   document.getElementById("myDropdown").classList.toggle("show");
 }
+
+window.onclick = function (event){
+  if (!event.target.matches('.icono_header2')) {
+    if (document.getElementById("myDropdown").classList.contains('show')) {
+      document.getElementById("myDropdown").classList.remove('show')
+    }
+  }
+}
