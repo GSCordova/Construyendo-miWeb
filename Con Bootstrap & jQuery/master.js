d26af5b60cdf7bc7771c368f1a24bf449f1d073a
Gianmarco <GSCordova7@gmail.com>
2018-01-23 16:57:23 +0100

    Añadiendo interactividad al boton

--- Con Bootstrap & jQuery/master.js
+++ Con Bootstrap & jQuery/master.js
@@ -1,8 +1,36 @@
-function init(){
-  $('#boton').click(function(){
-    alert("Hola");
-  })
-};
 
+/*
+let drop = document.getElementById('myDropdown');
+*/
+function myFunction(){
+  dropdown.classList.toggle("show");
+  dropdown.style.transition = 'all 2s'
+}
+/*
+window.onclick = function (event){
+  if (!event.target.matches('.icono_header2')) {
+    if (drop.classList.contains('show')) {
+      drop.classList.remove('show')
+    }
+  }
+}
+*/
+/*
+let solucion = "false"
 
-$(document).ready(init)
+$('#boton').click(function(){
+  return solucion = boton.getAttribute('aria-expanded')
+  if (solucion === "true") {
+      console.log("clickado cuando esta abierto")
+    }
+})*/
+
+window.onclick = function(event){
+  if (!event.target.matches('.pepe')){
+    if (dropdown.classList.contains('show')) {
+      dropdown.classList.remove('show');
+    }
+  }
+}
+
+let dropdown = document.getElementById('navbarNavAltMarkup')
