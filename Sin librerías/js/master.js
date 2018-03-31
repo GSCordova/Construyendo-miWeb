ccb70005ad6011f10f979048923bd07d6dbf5ce7
Gianmarco <GSCordova7@gmail.com>
2018-03-31 15:18:28 +0200

    Terminado

--- Sin librerías/js/master.js
+++ Sin librerías/js/master.js
@@ -10,22 +10,34 @@ window.onclick = function (event){
       document.getElementById("myDropdown").classList.remove('show')
     }
   }
-}
+};
 
-// document.getElementById('secreto').addEventListener('click', ()=>{
-//   console.log('Condiciones del contrato de novios:')
-//   console.log('1-Ser sinceros (contarnos todo).');
-//   console.log('2-Darnos amor siempre (mimitos, coito humano).');
-//   console.log('3-Hacer planes divertidos (no caer en la rutina.)');
-//   console.log('4-Cuidarnos');
-//   console.log('5-Hacer lo imposible por seguir juntos.');
-//   console.log('6-Fidelidad máxima.');
-//   console.log('Gianmarco pide:');
-//   console.log('7-No cambies nunca, me encanta cómo eres y cómo me miras.');
-//   console.log('8-No me hagas daño.');
-//   console.log('Patricia pide:');
-//   console.log('9-Sigas tratándome igual de bien, porque eres monísimo y adorable.');
-//   console.log('10-No dejar de vernos jamás');
-//   console.log('Anexo:');
-//   console.log('11-No dejarnos nunca.');
-// })
+
+var acumulador = '';
+
+document.addEventListener('keyup', (ev) => {
+  acumulador += ev.key
+  if (acumulador.includes('patricia') || acumulador.includes('amor') 
+  || acumulador.includes('te quiero') || acumulador.includes('tequiero') 
+  || acumulador.includes('nosotros') || acumulador.includes('pringada')
+  || acumulador.includes('gianmarco')){
+    alert(`
+      Condiciones del contrato de novios:
+      1-Ser('1- sinceros (contarnos todo).
+      2-Darnos amor siempre (mimitos, coito humano).
+      3-Hacer planes divertidos (no caer en la rutina.
+      4-Cuidarnos
+      5-Hacer lo imposible por seguir juntos.
+      6-Fidelidad máxima.
+      Gianmarco pide:
+      7-No cambies nunca, me encanta cómo eres y cómo me miras.
+      8-No me hagas daño.
+      Patricia pide:
+      9-Sigas tratándome igual de bien, porque eres monísimo y adorable.
+      10-No dejar de vernos jamás
+      Anexo:
+      11-No dejarnos nunca.
+    `)
+    acumulador = ''
+  }
+});
